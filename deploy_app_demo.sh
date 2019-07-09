64c015745dcc1eee08774d29d19c919490bd75e8
Andrew S <andrews@airbeem.com>
2019-07-09 11:56:03 +0300

    [ADD] change image to openjdk:8-jre-alpine

--- deploy_app_demo.sh
+++ deploy_app_demo.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 file="./src/main/resources/application.properties"
 
 version=$(grep -i 'info.build.version' $file  | cut -f2 -d'=')
 
 echo Version of Json API: $version
 
-apk add --no-cache openjdk8
 ./gradlew build -Dspring.profiles.active=docker
 java -jar ./build/libs/$version.jar  --spring.profiles.active=docker
